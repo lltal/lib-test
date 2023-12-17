5665da72b8225474597961338d38b60f4c706bbb
Alexandr Litvinov <litvinov_0104@mail.ru>
2023-12-17 21:29:47 +0300

    add service interfaces

--- src/main/java/com/github/lltal/testlibbot/services/CalculationCommandDataService.java
+++ src/main/java/com/github/lltal/testlibbot/services/CalculationCommandDataService.java
@@ -2,7 +2,11 @@ package com.github.lltal.testlibbot.services;
 
 
 import com.github.lltal.testlibbot.model.domain.CalculateCommandData;
+import com.github.lltal.testlibbot.model.domain.UserData;
 
+/**
+ * Организует взаимодействие с бд при обработке {@link CalculateCommandData}
+ */
 public interface CalculationCommandDataService {
 
     /**
